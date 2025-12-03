f7672d89a1f5a0c5a3ae26f7bd296f68a00231dd
ahmed mohamed <ahmed100119@hotmail.com>
2025-12-03 03:32:54 +0200

    testing the githubaction

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -2,7 +2,7 @@ class UserService:
     def __init__(self):
         self.users = [
             {"id": 1, "name": "John Doe"},
-            {"id": 2, "name": "Jane Doe"}
+            {"id": 2, "name": "ahmed abdelkhalek"}
         ]
 
     def get_users(self):
