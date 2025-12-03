527d4abc4b6b9027644bfbe85ea8cf1c0c1f1c59
ahmed mohamed <ahmed100119@hotmail.com>
2025-12-03 03:48:47 +0200

    added prometheus to the helm chart

--- argocd_install.sh
+++ argocd_install.sh
@@ -2,4 +2,5 @@ helm repo add argo https://argoproj.github.io/argo-helm
 helm repo update
 kubectl create namespace argocd
 helm install argocd argo/argo-cd -n argocd --set server.service.type=LoadBalancer
-kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 --decode
+password=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 --decode)
+echo "ArgoCD password: $password"
